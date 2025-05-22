81fd0ee0471ef348ca1bdf57e25a326bed18c215
Milena <120755750+MilenaAlm@users.noreply.github.com>
2025-05-22 11:52:08 +0300

    top_10_total_income.csv
    Отчет о десятке лучших продавцов (данные о продавце, суммарная выручка с проданных товаров и количество проведенных сделок)

--- queries.sql
+++ queries.sql
@@ -1,2 +1,18 @@
 "customers_count"
 19759
+
+
+
+  
+select 
+concat(first_name, ' ', last_name) as seller ,
+COUNT (sales_id) as operations,
+ROUND (SUM (quantity * price), 0) as income
+from sales s 
+left join products p 
+on s.product_id = p.product_id
+left join employees e 
+on s.sales_person_id= e.employee_id
+group by first_name, last_name
+order by income desc
+limit 10
