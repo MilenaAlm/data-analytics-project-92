29bdf9d4948740e3dc0585be83604acc5f9e5f01
Milena <120755750+MilenaAlm@users.noreply.github.com>
2025-05-22 15:59:14 +0300

    lowest_average_income.csv
    Информацию о продавцах, чья средняя выручка за сделку меньше средней выручки за сделку по всем продавцам.

--- queries.sql
+++ queries.sql
@@ -16,3 +16,25 @@ on s.sales_person_id= e.employee_id
 group by first_name, last_name
 order by income desc
 limit 10
+
+
+
+  
+WITH overall_avg AS (
+    SELECT AVG(quantity * price) AS avg_income
+    FROM sales s
+    JOIN products p ON s.product_id = p.product_id
+)
+select 
+concat(first_name, ' ', last_name) as seller ,
+ROUND(AVG(quantity * price), 0) AS average_income
+from sales s 
+left join products p 
+on s.product_id = p.product_id
+left join employees e 
+on s.sales_person_id= e.employee_id
+group by first_name, last_name
+HAVING 
+    AVG(quantity * price) < (SELECT avg_income FROM overall_avg)
+ORDER BY 
+    average_income ASC;
